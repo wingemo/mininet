158b085a0f6ff207901d294df04de658ba323624
Wingemo <philip.wingemo@outlook.com>
2022-10-06 23:38:05 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestNetwork( Topo ):
   def _init_( self ):
       Topo._init_( self )
       
-      #
+      # Hosts to add 
       h1 = self.addHost( 'h1' )
       h2 = self.addHost( 'h2' )
       h3 = self.addHost( 'h3' )
@@ -16,13 +16,13 @@ class TestNetwork( Topo ):
       h7 = self.addHost( 'h7' )
       h8 = self.addHost( 'h8' )
       
-      #
+      # Switchs to add 
       s1 = self.addSwitch( 's1' )
       s2 = self.addSwitch( 's2' )
       s3 = self.addSwitch( 's3' )
       s4 = self.addSwitch( 's4' )
       
-      #
+      # Links to add
       self.addLink(h1, s1)
       self.addLink(h2, s1)
       self.addLink(h3, s2)
@@ -31,7 +31,6 @@ class TestNetwork( Topo ):
       self.addLink(h6, s3)
       self.addLink(h7, s3)
       self.addLink(h8, s3)
-      
       self.addLink(s1, s3)
       self.addLink(s2, s3)
       self.addLink(s3, s4)
