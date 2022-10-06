26a60c5a35fba958d40105fc765d2f7edc1b9201
Wingemo <philip.wingemo@outlook.com>
2022-10-06 22:49:06 +0200

    Update test12.py

--- test12.py
+++ test12.py
@@ -17,7 +17,6 @@ list_host = [h1, h2, h3, h4, h5, h6, h7, h8]
 e1 = OVSSwitch("e1", inNamespace=False)
 e2 = OVSSwitch("e2", inNamespace=False)
 e3 = OVSSwitch("e3", inNamespace=False)
-e4 = OVSSwitch("e4", inNamespace=False)
 
 # Aggregation switches
 a1 = OVSSwitch("a1", inNamespace=False)
@@ -34,7 +33,6 @@ ct = Controller("ct", inNamespace=False)
 Link(e1, a1)
 Link(e2, a1)
 Link(e3, a2)
-Link(e4, a2)
 
 # Link between aggregation switches
 # and core switch
@@ -44,12 +42,10 @@ Link(a2, c1)
 # Links between hosts and edge switches
 Link(h1, e1)
 Link(h2, e1)
-Link(h3, e2)
+Link(h3, e1)
 Link(h4, e2)
-Link(h5, e3)
+Link(h5, e2)
 Link(h6, e3)
-Link(h7, e4)
-Link(h8, e4)
 
 # Set the IP address for the interface
 h1.setIP("10.0.0.1/24")
@@ -58,8 +54,6 @@ h3.setIP("10.0.0.3/24")
 h4.setIP("10.0.0.4/24")
 h5.setIP("10.0.0.5/24")
 h6.setIP("10.0.0.6/24")
-h7.setIP("10.0.0.7/24")
-h8.setIP("10.0.0.8/24")
 
 # Start up controller
 ct.start()
@@ -68,26 +62,20 @@ ct.start()
 e1.start([ct])
 e2.start([ct])
 e3.start([ct])
-e4.start([ct])
 a1.start([ct])
 a2.start([ct])
 c1.start([ct])
 
 # Test of reachability (for every node)
-def reachability_test():
-    for reciver in list_host:
-        if h1 != reciver:
-            print(h1.cmd("ping -c1", reciver.IP()))
+for reciver in list_host:
+    if h1 != reciver:
+        print(h1.cmd("ping -c1", reciver.IP()))
 
-# Test of reachability (core switch stopped)
-c1.stop()
-reachability_test()
 
 # Stop the OVS OpenFlow switches
 e1.stop()
 e2.stop()
 e3.stop()
-e4.stop()
 a1.stop()
 a2.stop()
 
